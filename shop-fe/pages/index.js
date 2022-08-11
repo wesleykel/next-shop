b083444fa1856c4c6c388ef5668387d1eecf25e9
Wesley Kelly <wesley.kelly@lego.com>
2022-08-11 13:28:39 +0100

    graph client connected

--- shop-fe/pages/index.js
+++ shop-fe/pages/index.js
@@ -1,12 +1,59 @@
 
+import Link from "next/link"
+import { gql} from "@apollo/client"
+import client from "../apollo-client"
 import { Title } from '../components/styled.title'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+
+
+
+
+
+
+export default function Home({products}) {
+  //console.log(products)
   return (
     <div className={styles.container}>
     
 <Title>Shop</Title>
+<Link href="admin"><a>Admin</a></Link>
+{products.map((item)=>
+<h1>{item.product_Name}</h1>
+)}
+
     </div>
   )
+}
+
+export async function getStaticProps(){
+
+
+
+  const { data } = await client.query({
+
+    
+query: gql`
+
+query GetAllProducts {
+  getAllProducts {
+    price
+    product_Name
+    in_Stock
+    item_Type
+    image
+  }
+}
+
+
+`,
+});
+
+return {
+
+  props:{
+
+    products:data.getAllProducts,
+  },
+};
 }
---
+++ shop-fe/pages/server-side.js
@@ -0,0 +1,35 @@
+import { gql} from "@apollo/client"
+import client from "../apollo-client"
+
+export async function getStaticProps(){
+
+
+
+   /* const { data } = await client.query({
+  
+      
+  query: gql`
+  
+  query GetAllProducts {
+    getAllProducts {
+      price
+      product_Name
+      in_Stock
+      item_Type
+      image
+    }
+  }
+  
+  
+  `,
+  });
+  
+  return {
+  
+    props:{
+  
+      products:data.getAllProducts,
+    },
+  };
+  }
+  */
